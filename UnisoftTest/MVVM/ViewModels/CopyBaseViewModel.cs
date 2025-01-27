7d46b6527da8c9efb07b9ae9bdc02ba1f0afbf75
scule <scule>
2025-01-27 19:12:35 +0100

    CBVM dodanie opcji wykonywania skryptu cmd

--- UnisoftTest/MVVM/ViewModels/CopyBaseViewModel.cs
+++ UnisoftTest/MVVM/ViewModels/CopyBaseViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using UniTest.MVVM.Models;
-using Windows.Media.Capture;
+
 
 namespace UnisoftTest.MVVM.ViewModels
 {
@@ -17,9 +18,13 @@ namespace UnisoftTest.MVVM.ViewModels
         public CopyBaseScripts CurrentScript { get; set; }
 
         public ICommand RunScript => new Command(RunCopyBaseScript);
+        public string ResultEditor { get; set; }
+        public bool ActIndIsRunning { get; set; }
+
 
         public CopyBasePageViewModel()
         {
+
             Refresh();
         }
 
@@ -38,9 +43,54 @@ namespace UnisoftTest.MVVM.ViewModels
 
             var currentCopyBaseScript = obj as CopyBaseScripts;
 
-            string txt = currentCopyBaseScript.CopyBaseScript;
+            string txtScript = currentCopyBaseScript.CopyBaseScript;
 
+            //ResultEditor = txtScript;
+            try
+            {
+                // Włącz ikonę ładowania
+                LoadingIcon(true);
 
+                // Wykonaj ciężkie zadanie w tle
+                await Task.Run(() =>
+                {
+                    // Symulacja ciężkiego zadania
+                    Process process = new Process();
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.Arguments = $"/c {txtScript}";
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.RedirectStandardError = true;
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
+
+                    process.Start();
+
+                    // Czytaj dane wyjściowe na bieżąco
+                    while (!process.StandardOutput.EndOfStream)
+                    {
+                        string line = process.StandardOutput.ReadLine();
+                        ResultEditor += line + Environment.NewLine; // Dodanie do wyniku
+                        //PropertyChanged(nameof(ResultEditor)); // Powiadomienie o zmianie
+                    }
+
+                    process.WaitForExit();
+                });
+            }
+            catch (Exception ex)
+            {
+                ResultEditor = $"Błąd: {ex.Message}";
+            }
+            finally
+            {
+                // Wyłącz ikonę ładowania
+                LoadingIcon(false);
+            }
         }
+
+        public void LoadingIcon(bool isRunning)
+        {
+            ActIndIsRunning = isRunning;
+        }
+
     }
 }
