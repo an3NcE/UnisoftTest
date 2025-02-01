e16bc675ba4e9be2be5173a408ef48db4bbe2954
scule <scule>
2025-02-01 03:01:14 +0100

    Poprawa wygląda nawigacji

--- UnisoftTest/AppShell.xaml.cs
+++ UnisoftTest/AppShell.xaml.cs
@@ -4,15 +4,30 @@ namespace UnisoftTest
 {
     public partial class AppShell : Shell
     {
+        public bool isAdministrator { get; set; }
         public AppShell()
         {
             InitializeComponent();
+            BindingContext = this;
             //Routing.RegisterRoute("ResultPageHome", typeof(ResultPage)); // Rejestracja trasy dla ResultPage
             //Routing.RegisterRoute("ConfigurationPageRoute", typeof(ConfigurationPage)); // Rejestracja trasy dla ConfigurationPage
             App.BaseRepo.AddOrUpdateAppAdministrator(true);
-            Routing.RegisterRoute("ResultPage", typeof(ResultPage));
+            isAdministrator = false;
         }
 
-       
+        private void SetAdministrator(object sender, EventArgs e)
+        {
+            if (isAdministrator == false)
+            {
+                isAdministrator = true;
+                lblAdministrator.Text = "Jesteś Administratorem! :)";
+            }
+            else
+            {
+                isAdministrator = false;
+                lblAdministrator.Text = "Czy jesteś Administratorem?";
+            }
+            
+        }
     }
 }
