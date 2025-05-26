1e342a4432efb3f4337e1ace12e79a143a5be009
scule <scule>
2025-05-26 16:34:18 +0200

    AESCredentialManager na androida

--- UnisoftTest/MVVM/Services/AesCredentialManager.cs
+++ UnisoftTest/MVVM/Services/AesCredentialManager.cs
@@ -1,10 +1,11 @@
-﻿using Meziantou.Framework.Win32;
+﻿
+using Meziantou.Framework.Win32;
 using System.Security.Principal;
-using System.Text;
 
-#if WINDOWS
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Maui.Controls;
-#endif
+using Microsoft.Maui.Storage;
 
 namespace UnisoftTest.Services
 {
@@ -15,12 +16,19 @@ namespace UnisoftTest.Services
 
         public static bool CredentialsExist()
         {
+#if WINDOWS
             return CredentialManager.ReadCredential(AesKeyTarget) != null &&
                    CredentialManager.ReadCredential(AesIvTarget) != null;
+#else
+            var key = SecureStorage.Default.GetAsync(AesKeyTarget).GetAwaiter().GetResult();
+            var iv = SecureStorage.Default.GetAsync(AesIvTarget).GetAwaiter().GetResult();
+            return key != null && iv != null;
+#endif
         }
 
         public static (string Key, string IV) GetCredentials()
         {
+#if WINDOWS
             var keyCred = CredentialManager.ReadCredential(AesKeyTarget);
             var ivCred = CredentialManager.ReadCredential(AesIvTarget);
 
@@ -28,10 +36,20 @@ namespace UnisoftTest.Services
                 throw new Exception("Brak zapisanych poświadczeń AES.");
 
             return (keyCred.Password, ivCred.Password);
+#else
+            var key = SecureStorage.Default.GetAsync(AesKeyTarget).GetAwaiter().GetResult();
+            var iv = SecureStorage.Default.GetAsync(AesIvTarget).GetAwaiter().GetResult();
+
+            if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(iv))
+                throw new Exception("Brak zapisanych poświadczeń AES.");
+
+            return (key, iv);
+#endif
         }
 
         public static void SaveCredentials(string key, string iv)
         {
+#if WINDOWS
             EnsureRunningAsAdministrator(); // ⛔ wymóg administratora
 
             CredentialManager.WriteCredential(
@@ -45,14 +63,19 @@ namespace UnisoftTest.Services
                 "aes_UniToolbox",
                 iv,
                 CredentialPersistence.LocalMachine);
+#else
+            SecureStorage.Default.SetAsync(AesKeyTarget, key).Wait();
+            SecureStorage.Default.SetAsync(AesIvTarget, iv).Wait();
+#endif
         }
-
+#if WINDOWS
         public static bool IsRunningAsAdministrator()
         {
             var identity = WindowsIdentity.GetCurrent();
             var principal = new WindowsPrincipal(identity);
             return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
+
 
         public static void EnsureRunningAsAdministrator()
         {
@@ -62,8 +85,11 @@ namespace UnisoftTest.Services
                     "Aplikacja musi być uruchomiona jako administrator, aby zapisać poświadczenia systemowe (LocalComputer).");
             }
         }
+#else
+        public static void EnsureRunningAsAdministrator() { }
+#endif
 
-//#if WINDOWS
+        //#if WINDOWS
         public static async Task EnsureAsync()
         {
             if (!CredentialsExist())
